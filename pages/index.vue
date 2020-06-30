a3d9b992b50f30e2307b2ec7abbdfe35f22b9a5e
luml <1520622848@qq.com>
2020-06-30 12:54:47 +0800

    feat(apply header, sider, main)

--- pages/index.vue
+++ pages/index.vue
@@ -1,160 +1,203 @@
 <template>
-  <div class="container">
-    <div>
-      <!-- <logo /> -->
-      <h1 class="title">
-        nuxt-ui-element
-      </h1>
-      <h2 class="subtitle">
-        My prime Nuxt.js project
-        <el-popover
-          placement="right-start"
-          title="Dingding"
-          width="200"
-          trigger="hover"
-          content="How do you feel when you see this is page not dope what's so ever"
-        >
-          <el-button slot="reference"><i class="el-icon-bell"></i></el-button>
-        </el-popover>
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          style="pointer-events: none"
-        >
-          GitHub
-        </a>
-      </div>
-      <div class="new-div">
-        <el-button @click="open">A Message Button</el-button>
-        <el-select
-          @change="showFruit"
-          v-model="value1"
-          name="source"
-          placeholder="favorite fruit"
-        >
-          <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          >
-          </el-option>
-        </el-select>
-        <el-input
-          v-model="value2"
-          placeholder="please give me some money"
-          clearable
-          @focus="showMeTheMoney($event)"
-          ref="inputRef"
-        ></el-input>
-        <el-input-number
-          v-model="value5"
-          @change="handleChange"
-          :min="1"
-          :max="10"
-          label="描述文字"
-        ></el-input-number>
-        <el-badge :value="100" class="item" type="primary"
-          ><el-button @click="countComments(100)" size="small"
-            >Comments</el-button
-          ></el-badge
-        >
-        <el-rate
-          v-model="value6"
-          show-text
-          :texts="moods"
-          @change="fantastic"
-        ></el-rate>
-      </div>
-      <div class="block">
-        <el-slider
-          v-model="value3"
-          :marks="marks"
-          :format-tooltip="formatTooltip"
-          vertical
-          height="100px"
-        ></el-slider>
-        <el-calendar
-          class="calendar"
-          v-model="value4"
-          :range="['2020-06-29', '2020-07-05']"
-        ></el-calendar>
-        <div class="tip-tool">
-          <el-tooltip
-            class="item"
-            effect="light"
-            content="Let's ride this cute boat to his heart TODAY`"
-            placement="left-end"
-          >
-            <el-button>🛶</el-button>
-          </el-tooltip>
+  <el-container>
+    <el-header
+      height="50px"
+      style="background-color: rgb(238, 241, 246);display: flex;justify-content: center;align-items: center"
+      >TO KILL THE BOREDOM</el-header
+    >
+    <el-container>
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1', '2']">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message">🍑</i></template>
+            <el-menu-item-group>
+              <template slot="title">🥦</template>
+              <el-menu-item index="1-1">🥦</el-menu-item>
+              <el-menu-item index="1-2">🥦</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-message">🍎</i></template>
+            <el-menu-item-group>
+              <template slot="title">🥬</template>
+              <el-menu-item index="2-1">🥬</el-menu-item>
+              <el-menu-item index="2-2">🥬</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <div class="container">
+          <div>
+            <!-- <logo /> -->
+            <h1 class="title">
+              nuxt-ui-element
+            </h1>
+            <h2 class="subtitle">
+              My prime Nuxt.js project
+              <el-popover
+                placement="right-start"
+                title="Dingding"
+                width="200"
+                trigger="hover"
+                content="How do you feel when you see this is page not dope what's so ever"
+              >
+                <el-button slot="reference"
+                  ><i class="el-icon-bell"></i
+                ></el-button>
+              </el-popover>
+            </h2>
+            <div class="links">
+              <a
+                href="https://nuxtjs.org/"
+                target="_blank"
+                class="button--green"
+              >
+                Documentation
+              </a>
+              <a
+                href="https://github.com/nuxt/nuxt.js"
+                target="_blank"
+                class="button--grey"
+                style="pointer-events: none"
+              >
+                GitHub
+              </a>
+            </div>
+            <div class="new-div">
+              <el-button @click="open">A Message Button</el-button>
+              <el-select
+                @change="showFruit"
+                v-model="value1"
+                name="source"
+                placeholder="favorite fruit"
+              >
+                <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                >
+                </el-option>
+              </el-select>
+              <el-input
+                v-model="value2"
+                placeholder="please give me some money"
+                clearable
+                @focus="showMeTheMoney($event)"
+                ref="inputRef"
+              ></el-input>
+              <el-input-number
+                v-model="value5"
+                @change="handleChange"
+                :min="1"
+                :max="10"
+                label="描述文字"
+              ></el-input-number>
+              <el-badge :value="100" class="item" type="primary"
+                ><el-button @click="countComments(100)" size="small"
+                  >Comments</el-button
+                ></el-badge
+              >
+              <el-rate
+                v-model="value6"
+                show-text
+                :texts="moods"
+                @change="fantastic"
+              ></el-rate>
+            </div>
+            <div class="block">
+              <el-slider
+                v-model="value3"
+                :marks="marks"
+                :format-tooltip="formatTooltip"
+                vertical
+                height="100px"
+              ></el-slider>
+              <el-calendar
+                class="calendar"
+                v-model="value4"
+                :range="['2020-06-29', '2020-07-05']"
+              ></el-calendar>
+              <div class="tip-tool">
+                <el-tooltip
+                  class="item"
+                  effect="light"
+                  content="Let's ride this cute boat to his heart TODAY`"
+                  placement="left-end"
+                >
+                  <el-button>🛶</el-button>
+                </el-tooltip>
+              </div>
+              <el-breadcrumb separator-class="el-icon-arrow-right">
+                <el-breadcrumb-item :to="{ path: '/' }"
+                  >Maybe</el-breadcrumb-item
+                >
+                <el-breadcrumb-item><a href="/">Today</a></el-breadcrumb-item>
+                <el-breadcrumb-item class="no">Not</el-breadcrumb-item>
+                <el-breadcrumb-item><a href="/">The</a></el-breadcrumb-item>
+                <el-breadcrumb-item class="day">Day</el-breadcrumb-item>
+              </el-breadcrumb>
+              <div class="time-picker">
+                <el-time-select
+                  v-model="value7"
+                  :picker-options="{
+                    start: '09:00',
+                    step: '00:15',
+                    end: '18:00'
+                  }"
+                  placeholder="pick a time"
+                ></el-time-select>
+              </div>
+              <el-color-picker
+                v-model="color"
+                show-alpha
+                :predefine="predefineColors"
+                @change="changeSomeBgc"
+              ></el-color-picker>
+            </div>
+            <div>
+              <el-progress
+                :text-inside="true"
+                :stroke-width="26"
+                :percentage="70"
+              ></el-progress>
+              <el-progress
+                :text-inside="true"
+                :stroke-width="24"
+                :percentage="100"
+                status="success"
+              ></el-progress>
+              <el-progress
+                :text-inside="true"
+                :stroke-width="22"
+                :percentage="80"
+                status="warning"
+              ></el-progress>
+              <el-progress
+                :text-inside="true"
+                :stroke-width="20"
+                :percentage="50"
+                status="exception"
+              ></el-progress>
+            </div>
+            <div class="footer">
+              <el-link href="https://element.eleme.io" target="_blank"
+                >Default💘</el-link
+              >
+              <el-link type="primary">Primary💙</el-link>
+              <el-link type="success">Success💚</el-link>
+              <el-link type="warnning">Warning💛</el-link>
+              <el-link type="danger">Danger💔</el-link>
+              <el-link type="info" href="https://dreamy-elio-gatsby.netlify.com"
+                >Check out💝</el-link
+              >
+            </div>
+          </div>
         </div>
-        <el-breadcrumb separator-class="el-icon-arrow-right">
-          <el-breadcrumb-item :to="{ path: '/' }">Maybe</el-breadcrumb-item>
-          <el-breadcrumb-item><a href="/">Today</a></el-breadcrumb-item>
-          <el-breadcrumb-item class="no">Not</el-breadcrumb-item>
-          <el-breadcrumb-item><a href="/">The</a></el-breadcrumb-item>
-          <el-breadcrumb-item class="day">Day</el-breadcrumb-item>
-        </el-breadcrumb>
-        <div class="time-picker">
-          <el-time-select
-            v-model="value7"
-            :picker-options="{ start: '09:00', step: '00:15', end: '18:00' }"
-            placeholder="pick a time"
-          ></el-time-select>
-        </div>
-        <el-color-picker
-          v-model="color"
-          show-alpha
-          :predefine="predefineColors"
-          @change="changeSomeBgc"
-        ></el-color-picker>
-      </div>
-      <div>
-        <el-progress
-          :text-inside="true"
-          :stroke-width="26"
-          :percentage="70"
-        ></el-progress>
-        <el-progress
-          :text-inside="true"
-          :stroke-width="24"
-          :percentage="100"
-          status="success"
-        ></el-progress>
-        <el-progress
-          :text-inside="true"
-          :stroke-width="22"
-          :percentage="80"
-          status="warning"
-        ></el-progress>
-        <el-progress
-          :text-inside="true"
-          :stroke-width="20"
-          :percentage="50"
-          status="exception"
-        ></el-progress>
-      </div>
-      <div class="footer">
-        <el-link href="https://element.eleme.io" target="_blank"
-          >Default💘</el-link
-        >
-        <el-link type="primary">Primary💙</el-link>
-        <el-link type="success">Success💚</el-link>
-        <el-link type="warnning">Warning💛</el-link>
-        <el-link type="danger">Danger💔</el-link>
-        <el-link type="info" href="https://dreamy-elio-gatsby.netlify.com"
-          >Check out💝</el-link
-        >
-      </div>
-    </div>
-  </div>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
