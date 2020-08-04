21739c5b40d3766728452e5f9463571d15b9900b
lumeiling <1520622848@qq.com>
2020-08-04 21:41:51 +0800

    feat: form

--- pages/second/index.vue
+++ pages/second/index.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="box">
-    <h3>Hey there. I'm a bird.</h3>
     <div class="blcok">
+      <h3>Hey there. I'm a bird.</h3>
       <!-- <span class="demonstration"></span> -->
       <el-image style="width: 600px; height: 500px" :src="url"></el-image>
     </div>
-    <el-button @click="goBack">Go Back</el-button>
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="name">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="region">
+        <el-select v-model="form.region" placeholder="please pick up a region">
+          <el-option label="Beijing" value="bj"></el-option>
+          <el-option label="Shanghai" value="sh"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="delivery">
+        <el-switch v-model="form.delivery"></el-switch>
+      </el-form-item>
+      <el-form-item label="type">
+        <el-checkbox-group v-model="form.type">
+          <el-checkbox label="food" name="type"></el-checkbox>
+          <el-checkbox label="streetVendor" name="type"></el-checkbox>
+          <el-checkbox label="offline" name="type"></el-checkbox>
+          <el-checkbox label="branding" name="type"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Confirm</el-button>
+        <el-button>Cancel</el-button>
+      </el-form-item>
+    </el-form>
+    <el-button @click="goBack">⬅️</el-button>
   </div>
 </template>
 
@@ -16,13 +42,23 @@ export default {
       // src: "../../assets/hotsummer.JPG",
       // fit: "contain",
       url:
-        "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+        "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+      form: {
+        name: "",
+        region: "",
+        delivery: false,
+        type: ["food"]
+      }
     };
   },
 
   methods: {
     goBack() {
       this.$router.go("-1");
+    },
+
+    onSubmit() {
+      console.log("submit!");
     }
   }
 };
@@ -30,6 +66,7 @@ export default {
 
 <style scoped>
 .box {
+  display: flex;
   margin: 2rem;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
 }
